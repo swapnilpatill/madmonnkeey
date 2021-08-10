96fd389bc7714f59fd0f13d05cbb5e31b86e50c1
Swapnil Patil <sswapnil.patils@gmail.com>
2021-08-11 00:08:33 +0530

    gulp started

--- wp-content/themes/madmonkeey/home-page-template.php
+++ wp-content/themes/madmonkeey/home-page-template.php
@@ -22,15 +22,25 @@ get_header(); ?>
 </ul>
 </header> 
 <div class="leftSection">
-    <span class="mianlogowarap">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/madmonkeeylogo.svg" class="mlogo" alt="">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/madmonkeeynamelogo.svg" class="namelogo" alt="">
-    </span>
-    <div id="countdown-gampang"></div>
+    <div class="wrapsecin">
+        <span class="mianlogowarap">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/madmonkeeylogo.svg" class="mlogo" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/madmonkeeynamelogo.svg" class="namelogo" alt="">
+        </span>
+        <div id="countdown-gampang"></div>
+    </div>
 </div>
 <div class="main">
      <section class="section">
-        
+        <h1>
+            COMING SOON
+        </h1>
+        <h2>welcome to Mad Monkeey</h2>
+        <p>We Are Launching the site very soon. Please Come Back Later.</p>
+        <div>
+            <input type="text">
+            <button type="submit">Subscribe Now</button>
+        </div>
      </section>
      <section class="section">
          page 2
