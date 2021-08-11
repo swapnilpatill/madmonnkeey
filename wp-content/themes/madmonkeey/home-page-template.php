2e7a3488e55f20ac56dd4af4623799f7c2c829df
Swapnil Patil <sswapnil.patils@gmail.com>
2021-08-11 14:05:57 +0530

    forms added

--- wp-content/themes/madmonkeey/home-page-template.php
+++ wp-content/themes/madmonkeey/home-page-template.php
@@ -43,9 +43,7 @@ get_header(); ?>
             </h1>
             <h2>welcome to Mad Monkeey</h2>
             <p>We Are Launching the site very soon. Please Come Back Later.</p>
-            
                 <?php echo do_shortcode( '[contact-form-7 id="8" title="Subscription"]' ); ?>
-                
              
             <div class="socialmed"></div>
         </div>
